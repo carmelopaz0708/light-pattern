84a5015ee90a33596e170dc6cc1dd16bb5669c16
Carmelo Paz <carmelopaz0708@gmail.com>
2021-09-25 21:15:17 +0800

    Removed Led class; reworked as structure

--- helper.h
+++ helper.h
@@ -5,24 +5,11 @@
 
 class Button
 {
-    int _pin;
-    bool _pressed = false;
+    bool m_pressed = false;
     bool debounce(bool reading);
-    public:
-        Button::Button(int pin);
-};
-
-class Led
-{
-    int _pin;
-    bool _on;
-    // Might include an extra private class variable relating class Button to class Led
-    public:
-        Led::Led(int pin);
-        void onAll();
-        void onDecrement();
-        void onIncrement();
-        void onRandom();
+public:
+    int m_pin;
+    Button::Button(int pin);
 };
 
 #endif
