f022d157895506981c29a88b501f3f93b3f9f3b2
Carmelo Paz <carmelopaz0708@gmail.com>
2021-09-25 21:14:39 +0800

    Updated file

--- helper.cpp
+++ helper.cpp
@@ -6,17 +6,11 @@
 Button::Button(int pin)
 {
     pinMode(pin, INPUT);
-    _pin = pin;
-}
-
-Led::Led(int pin)
-{
-    pinMode(pin, OUTPUT);
-    _pin = pin;
+    m_pin = pin;
 }
 
 bool Button::debounce(bool reading)
 {
-    digitalRead(_pin);              // Unfinished. Must tact on
+    digitalRead(m_pin);              // Unfinished. Must tact on
 }
 
