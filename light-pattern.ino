642c6a91125e27f423ce2a779c9c9175b218ad6f
Carmelo Paz <carmelopaz0708@gmail.com>
2021-09-26 17:49:57 +0800

    Changed classes to structures.

--- light-pattern.ino
+++ light-pattern.ino
@@ -1,44 +1,69 @@
-#include "helper.h"
+/*
+  Client file
+*/
 
-Button sw1(A0);
-Button sw2(A1);
-Button sw3(A2);
-Button sw4(A4);
-
-unsigned long sw_dbTime = 0;
-unsigned long db_delay = 100;
 const int LED_COUNT = 9;
+const int SW_COUNT = 4;
+int led[LED_COUNT] = {2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-struct Led
+const unsigned long db_delay = 50;
+bool pos_edge = HIGH;
+
+struct Button 
 {
-    int pins[LED_COUNT] = {2, 3, 4, 5, 6, 7, 8, 9, 10};
-    bool m_f_all = false;
-    bool m_f_dec = false;
-    bool m_f_inc = false;
-    bool m_f_ran = false;
-    void activateAll();
-    void activateDecrement();
-    void activateIncrement();
-    void activateRandom();
-} leds;
+	int m_pin;
+	bool m_pressed;
+
+	bool readAndDebounce(int pin)
+	{
+		unsigned long db_time;
+
+		int reading = digitalRead(pin);
+		if (reading == pos_edge)
+		{
+			db_time = millis();
+		}
+
+		if ((millis() - db_time) > db_delay)
+		{
+			if (reading == pos_edge)
+			{
+				return true;
+			} else return false;
+		}
+	};
+};
+
+struct Button sw1 = {14, false};	// 14~17 : A0~A3
+struct Button sw2 = {15, false};
+struct Button sw3 = {16, false};
+struct Button sw4 = {17, false};
 
 void setup() 
 {
-	for (int i = 0; i < sizeof(leds.pins); i++)
+	pinMode(sw1.m_pin, INPUT);
+	pinMode(sw2.m_pin, INPUT);
+	pinMode(sw3.m_pin, INPUT);
+	pinMode(sw4.m_pin, INPUT);
+
+	for (int i = 0; i < sizeof(led); i++)
 	{
-		pinMode(leds.pins[i], OUTPUT);
+		pinMode(led[i], OUTPUT);
 	}
+
+	Serial.begin(9600);
+	Serial.flush();
 }
 
 void loop() 
 {
-	// Test function. Can be deleted
-	if (digitalRead(sw1.m_pin) == HIGH)
+	// For testing
+	bool triggered = digitalRead(sw1.m_pin);
+	
+	if (triggered == HIGH)
 	{
-		for (int i = 0; i < LED_COUNT; i++)
-		{
-			digitalWrite(leds.pins[i], HIGH);
-			delay(500);
-		}
-	}
+		Serial.println(1);
+	} else Serial.println(0);
+
+	delay(500);
 }
