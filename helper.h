35ec3ebd218499d8b1e96c452cb6e4c02dce8e35
Carmelo Paz <carmelopaz0708@gmail.com>
2021-09-27 09:56:12 +0800

    Updated pin type initialization to uint8_t

--- helper.h
+++ helper.h
@@ -9,10 +9,13 @@ class Button
 {
 private:
     bool m_pressed;
-    int m_pin;
+    uint8_t m_pin;
 public:
-    Button(int pin);
+    Button(uint8_t pin);
     void read();
+
+    // Test function
+    void test();
 };
 
 class Led
