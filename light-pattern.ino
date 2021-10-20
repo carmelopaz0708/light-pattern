1f50d6232e6b022d0819db91f931ff8504789423
Carmelo Paz <carmelopaz0708@gmail.com>
2021-10-20 14:46:11 +0800

    Added led member functions in loop(). Renamed button inputs

--- light-pattern.ino
+++ light-pattern.ino
@@ -7,33 +7,37 @@
 // Set pins for LED
 uint8_t led_pins[9] = {2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-// Initialize pins with constructors. Pin 14 = A0, 15 = A1 ...
-Button sw1(14);
-Button sw2(15);
-Button sw3(16);
-Button sw4(17);
+// Set objects
+Button sw1(A0);
+Button sw2(A1);
+Button sw3(A2);
+Button sw4(A3);
 Led leds(led_pins);
 
-void setup()
-{
-    // Begin tests here
-    // Serial.begin(9600);
-    // Serial.flush();
-    // End tests here
-}
+void setup() {}
 
 void loop()
 {
-    // Proof of concept
     sw1.read();
-    if (sw1.m_pressed == true)
-    {
-        leds.activateAll();
-        // m_pin must be set to 0 after leds activate
-    }
+    sw2.read();
+    sw3.read();
 
-    // Begin tests here
-    // sw1.test();
-    // leds.test();
-    // End tests here
+    if (sw1.m_pressed)
+    {
+        sw1.m_pressed = !sw1.m_pressed;
+        leds.reset();
+        leds.activateAll();
+    } 
+    if (sw2.m_pressed)
+    {
+        sw2.m_pressed = !sw2.m_pressed;
+        leds.reset();
+        leds.activateDown();
+    }
+    if (sw3.m_pressed)
+    {
+        sw3.m_pressed = !sw3.m_pressed;
+        leds.reset();
+        leds.activateUp();
+    }
 }
